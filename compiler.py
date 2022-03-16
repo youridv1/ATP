14e1b6bdc4f2b27e118d09fb2891eda3e2e86b33
youridv1 <youri.devor@student.hu.nl>
2022-03-16 11:35:04 +0100

    Add string printing. No printing of strings inside variables yet

--- compiler.py
+++ compiler.py
@@ -4,8 +4,8 @@ import sys
 
 sys.setrecursionlimit(20000)  # core dumped at 21804
 
-def beginFile():
-    return ".cpu cortex-m0\n.align 2\n.text\n.global youriMain\nyouriMain:\n"
+def beginFile(dataSegment = ""):
+    return f".cpu cortex-m0\n.align 2\n\n{dataSegment}.text\n.global youriMain\n\nyouriMain:\n"
 
 def valueToRegister(value: Value, memory: dict, allRegisters: list): # puts an inline value in an available register 
   register = [x for x in allRegisters if x not in memory.values()][0]
@@ -14,12 +14,39 @@ def valueToRegister(value: Value, memory: dict, allRegisters: list): # puts an i
   memory[value.content] = register
   return assembly, memory
 
-def compileZegNa(toCompile, memory):
-    f = lambda x: f"mov r0, {memory[x.name]}\nbl printlnInteger\n" if type(x) == Variable else "oh fuck"
-    return "".join([f(x) for x in toCompile.args]), memory
+def stringToR0(value: Value, data: dict):
+  key = f"LIT{len(data)}"
+  data[key] = value.content
+  assembly = f"ldr r0, ={key}\n"
+  return assembly, data
+
+def intToR0(value):
+  content = int(value.content)
+  func, sign = ("mov", "#") if content < 256 else ("ldr", "=")
+  assembly = f"{func} r0, {sign}{content}\n"
+  return assembly
+
+def ZegNaHelper(arg, memory, data):
+    match arg:
+        case arg if type(arg) == Variable:
+            return f"mov r0, {memory[arg.name]}\nbl printlnInteger\n", memory, data
+        case arg if type(arg) == Value:
+            if '"' in arg.content:
+              assembly, data = stringToR0(arg, data)
+              return assembly+"bl printlnStr\n", memory, data
+            assembly = intToR0(arg)
+            return assembly+"bl printlnInteger\n", memory, data
+            
+
+def compileZegNa(toCompile, memory, data):
+    assembly, memory, data = ZegNaHelper(toCompile.args[0], memory, data)
+    assembly2 = ""
+    if rest := toCompile.args[1:]:
+        assembly2, memory, data = compileZegNa(Expression("zeg_na", len(rest), rest), memory, data)
+    return assembly+assembly2, memory, data
 
 # Returns string of assembly needed for this statement
-def compileExpression(toCompile: Expression, memory):
+def compileExpression(toCompile: Expression, memory, data):
     allRegisters = ["r1", "r2", "r3", "r4", "r5", "r6", "r7"]
     funcToAssem = {"produceer":"mul", "stapel":"add", "verklein":"sub"}
     match toCompile.function:
@@ -31,36 +58,43 @@ def compileExpression(toCompile: Expression, memory):
                 # for now we assume nothing ends up in memory and everything fits into registers
                 memAssem = ""
                 snd = memory[toCompile.args[1].name]
-            return memAssem + f"{funcToAssem[a]} {memory[toCompile.args[0].name]}, {snd}, {memory[toCompile.args[0].name]}\n", memory # "memory" will store the location of the data, so a register name or a memory address
+            return memAssem + f"{funcToAssem[a]} {memory[toCompile.args[0].name]}, {snd}, {memory[toCompile.args[0].name]}\n", memory, data # "memory" will store the location of the data, so a register name or a memory address
         case "stel":
             if type(toCompile.args[1]) == Value:
-                func, sign = ("mov", "#") if toCompile.args[1].content < 256 else ("ldr", "=")
-                immed = sign + str(toCompile.args[1].content)
-                register = [x for x in allRegisters if x not in memory.values()][0]
-                memory[toCompile.args[0].name] = register
-                return f"{func} {register}, {immed}\n", memory
+                  func, sign = ("mov", "#") if toCompile.args[1].content < 256 else ("ldr", "=")
+                  immed = sign + str(toCompile.args[1].content)
+                  register = [x for x in allRegisters if x not in memory.values()][0]
+                  memory[toCompile.args[0].name] = register
+                  return f"{func} {register}, {immed}\n", memory, data
         case "zeg_na":
-            return compileZegNa(toCompile, memory)
+            return compileZegNa(toCompile, memory, data)
         case _:
             print(":(")
             return
 
+def compileData(data):
+  helper = lambda x, y: f"{x}: .asciz {y}\n"
+  bruh = [helper(key, value) for key, value in data.items()]
+  return ".data\n\n" + "".join(bruh) + "\n"
 
-def compile(ast: list, memory: dict = {}):
-    assembly, memory = compileExpression(ast[0], memory)
+
+def compile(ast: list, memory: dict = {}, data: dict = {}):
+    assembly, memory, data = compileExpression(ast[0], memory, data)
     assembly2 = ""
     if rest := ast[1:]:
-        assembly2, memory = compile(rest, memory)
-    return assembly+assembly2, memory
+        assembly2, memory, data = compile(rest, memory, data)
+    return assembly+assembly2, memory, data
 
 
 def mainCompiler(fileName: str):
-    fileBegin = beginFile()
     push = "push { r1, r2, r3, r4, r5, r6, r7, lr }\n"
     pop = "pop { r1, r2, r3, r4, r5, r6, r7, pc }\n"
     x = parse(lex(f"{fileName}.yo"))
-    compiledCode, _ = compile(x)
+    compiledCode, _, data = compile(x)
     print(x)
+    print(data)
+    dataSegment = compileData(data)
+    fileBegin = beginFile(dataSegment)
     with open(f"{fileName}.S", 'w') as f:
         f.write(fileBegin + push + compiledCode + pop)
 
