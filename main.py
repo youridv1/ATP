703a6799bdb198e26f821c61afb4eb6152a163e5
youridv1 <youri.devor@student.hu.nl>
2022-04-07 16:34:02 +0200

    Improve cli

--- main.py
+++ main.py
@@ -3,21 +3,29 @@ import AST as a
 import interpreter as i
 import sys
 import compiler as cmp
+import os
 
 sys.setrecursionlimit(20000) # core dumped at 21804
 
-def main(compiler: bool, interpeter: bool) -> None:
+def main(compile: bool, interpet: bool) -> None:
     b = l.lex("youriMain.yo")
     x = a.parse(b)
-    if compiler:
+    if compile:
         cmp.mainCompiler("youriMain", x)
-    if interpeter:
+    if interpet:
         i.interpret(x)
     
 if __name__ == "__main__":
-    compiler = "compile" in sys.argv
-    interpreter = "interpret" in sys.argv
-    if compiler or interpreter:
-        main(compiler, interpreter)
-    else:
-        print("No option given\nOptions:\n- compile\n- interpret\nEnter both options if you want to do both\n")
+    compile = "compile" in sys.argv
+    interpret = "interpret" in sys.argv
+    clean = "clean" in sys.argv
+
+    if clean:
+        os.popen('find ./PlatformioProject/src -iname "*.S" -delete')
+
+    os.popen('cp PlatformioProject/lib/print.S PlatformioProject/src/')
+
+    if compile or interpret:
+        main(compile, interpret)
+    elif not clean:
+        print("No option given\nOptions:\n- compile\n- interpret\n- clean\nEnter multiple options if you want to\n")
